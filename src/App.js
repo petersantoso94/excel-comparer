11d9d666cc3f878e258adf2633278ce9cfb45ecd
petersantoso94 <petersantoso94@gmail.com>
2020-09-11 16:05:55 +0800

    Upgrade interface, remove draggable component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React, { useState } from "react";
-import readXlsxFile from "read-excel-file";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+import InputLabel from "@material-ui/core/InputLabel";
 import Paper from "@material-ui/core/Paper";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
-import DragSortableList from "react-drag-sortable";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import React, { useState } from "react";
+import readXlsxFile from "read-excel-file";
 import "./App.css";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </div>
+  );
+}
 
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
@@ -46,25 +64,41 @@ const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 850,
   },
+  mt10: {
+    marginTop: theme.spacing(1),
+  },
 }));
 function App() {
   const [file1Data, setfile1Data] = useState({});
   const [file2Data, setfile2Data] = useState({});
   const [selectedSheet1, setselectedSheet1] = useState("");
   const [selectedSheet2, setselectedSheet2] = useState("");
+  const [selectedColumn1, setselectedColumn1] = useState(0);
+  const [selectedColumn2, setselectedColumn2] = useState(0);
   const [diff1, setdiff1] = useState([]);
   const [diff2, setdiff2] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [tabValue, setTabValue] = React.useState(0);
   const headerFile1 = [];
   const headerFile2 = [];
   const sheetFile1Component = [];
   const sheetFile2Component = [];
+  const headerFile1Component = [];
+  const headerFile2Component = [];
   const classes = useStyles();
-  const FileOnChangeHandler = (evt, setFileFunction, setSelectedSheetFunction) => {
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+  const FileOnChangeHandler = (
+    evt,
+    setFileFunction,
+    setSelectedSheetFunction
+  ) => {
     let file1 = evt.target.files;
     if (!file1 || file1.length === 0) {
       return;
     }
-    setSelectedSheetFunction("")
+    setSelectedSheetFunction("");
     let sheetObject = {};
     readXlsxFile(file1[0], { getSheets: true }).then((sheets) => {
       sheets.forEach((sheet) => {
@@ -81,52 +115,35 @@ function App() {
       });
     });
   };
-  const pushSheetData = (key, sheetComponent) => {
+  const pushOptionData = (key, sheetComponent, value = null) => {
     sheetComponent.push(
-      <option value={key} key={key + uuidv4()}>
+      <option value={value != null ? value : key} key={key + uuidv4()}>
         {key}
       </option>
     );
   };
-  const selectSheetHandler = (evt, handler) => {
-    const sheet = evt.target.value;
-    handler(sheet);
+  const selectOnChangeHandler = (evt, handler) => {
+    // reset the diff on change
+    setdiff1([]);
+    setdiff2([]);
+    handler(evt.target.value);
   };
   const onSort = (sortedList) => {};
   const ComparingHandler = (evt) => {
+    setLoading(true);
     let diffs1 = file1Data[selectedSheet1].map((a) => [...a]);
     let diffs2 = file2Data[selectedSheet2].map((a) => [...a]);
-    let sameIdx1 = 0;
-    let sameIdx2 = 0;
-    headerFile1.forEach((head1, idx) => {
-      if (head1.rank === 0) {
-        sameIdx1 = idx;
-        return;
-      }
-    });
-    headerFile2.forEach((head2, idx) => {
-      if (head2.rank === 0) {
-        sameIdx2 = idx;
-        return;
-      }
-    });
     file1Data[selectedSheet1].forEach((fl1, id1) => {
       if (id1 > 0) {
         // only compare row data
         file2Data[selectedSheet2].forEach((fl2, id2) => {
-          if (id2 > 0 && fl1[sameIdx1] === fl2[sameIdx2]) {
+          if (id2 > 0 && fl1[selectedColumn1] === fl2[selectedColumn2]) {
             // only compare row data with same data in compareable column
-            headerFile1.forEach((head1, i1) => {
-              headerFile2.forEach((head2, i2) => {
-                if (
-                  head1.rank > 0 &&
-                  head2.rank > 0 &&
-                  head1.rank === head2.rank
-                ) {
-                  if (fl1[i1] == fl2[i2]) {
-                    diffs1[id1][i1] = "-";
-                    diffs2[id2][i2] = "-";
-                  }
+            headerFile1.forEach((_, i1) => {
+              headerFile2.forEach((_, i2) => {
+                if (fl1[i1] == fl2[i2]) {
+                  diffs1[id1][i1] = "-";
+                  diffs2[id2][i2] = "-";
                 }
               });
             });
@@ -136,38 +153,29 @@ function App() {
     });
     setdiff1([...diffs1]);
     setdiff2([...diffs2]);
+    setLoading(false);
   };
   // if there is file data, start to separate the sheet and header
   if (file1Data && file2Data) {
     Object.keys(file1Data).forEach((key) =>
-      pushSheetData(key, sheetFile1Component)
+      pushOptionData(key, sheetFile1Component)
     );
     Object.keys(file2Data).forEach((key) =>
-      pushSheetData(key, sheetFile2Component)
+      pushOptionData(key, sheetFile2Component)
     );
   }
   // if there is selected sheet
   if (selectedSheet1 && headerFile1.length === 0) {
     // 0 because the header always start with 0
-    file1Data[selectedSheet1][0].forEach((header) => {
-      headerFile1.push({
-        content: (
-          <ListItem button>
-            <ListItemText inset primary={header} />
-          </ListItem>
-        ),
-      });
+    file1Data[selectedSheet1][0].forEach((header, idx) => {
+      pushOptionData(`${header}`, headerFile1Component, idx);
+      headerFile1.push(header);
     });
   }
   if (selectedSheet2 && headerFile2.length === 0) {
-    file2Data[selectedSheet2][0].forEach((header) => {
-      headerFile2.push({
-        content: (
-          <ListItem button>
-            <ListItemText inset primary={header} />
-          </ListItem>
-        ),
-      });
+    file2Data[selectedSheet2][0].forEach((header, idx) => {
+      pushOptionData(`${header}`, headerFile2Component, idx);
+      headerFile2.push(header);
     });
   }
   return (
@@ -181,10 +189,15 @@ function App() {
       >
         <Grid item xs={6}>
           <Paper className={classes.paper}>
-            <p>File Excel 1</p>
+            <AppBar position="static" color="secondary">
+              <p>File Excel 1</p>
+            </AppBar>
             <input
+              className={classes.mt10}
               type="file"
-              onChange={(e) => FileOnChangeHandler(e, setfile1Data, setselectedSheet1)}
+              onChange={(e) =>
+                FileOnChangeHandler(e, setfile1Data, setselectedSheet1)
+              }
             ></input>
             <FormControl className={classes.formControl}>
               <InputLabel htmlFor="sheet-from-file1">
@@ -193,7 +206,7 @@ function App() {
               <Select
                 native
                 value={selectedSheet1}
-                onChange={(e) => selectSheetHandler(e, setselectedSheet1)}
+                onChange={(e) => selectOnChangeHandler(e, setselectedSheet1)}
                 inputProps={{
                   id: "sheet-from-file1",
                 }}
@@ -202,62 +215,39 @@ function App() {
                 {sheetFile1Component}
               </Select>
             </FormControl>
-            {headerFile1.length > 0 ? (
-              <List component="nav" aria-label="contacts">
-                <DragSortableList
-                  items={headerFile1}
-                  onSort={onSort}
-                  type="vertical"
-                />
-              </List>
+            {headerFile1Component.length > 0 ? (
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="column-from-file1">
+                  Column as key to compare:
+                </InputLabel>
+                <Select
+                  native
+                  value={selectedColumn1}
+                  onChange={(e) => selectOnChangeHandler(e, setselectedColumn1)}
+                  inputProps={{
+                    id: "column-from-file1",
+                  }}
+                >
+                  <option aria-label="None" value="" />
+                  {headerFile1Component}
+                </Select>
+              </FormControl>
             ) : (
               <></>
             )}
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    {diff1.map((row, idx) => {
-                      if (idx === 0) {
-                        let result = [];
-                        row.forEach((x) => {
-                          result.push(
-                            <TableCell key={uuidv4()}>{x}</TableCell>
-                          );
-                        });
-                        return result;
-                      }
-                    })}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {diff1.map((row, idx) => {
-                    let result = [];
-                    if (idx > 0) {
-                      result.push(
-                        <TableRow key={"1" + uuidv4()}>
-                          {row.map((x) => {
-                            result.push(
-                              <TableCell key={x + uuidv4()}>{x}</TableCell>
-                            );
-                          })}
-                        </TableRow>
-                      );
-
-                      return result;
-                    }
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
           </Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>
-            <p>File Excel 2</p>
+            <AppBar position="static" color="secondary">
+              <p>File Excel 2</p>
+            </AppBar>
             <input
+              className={classes.mt10}
               type="file"
-              onChange={(e) => FileOnChangeHandler(e, setfile2Data,setselectedSheet2)}
+              onChange={(e) =>
+                FileOnChangeHandler(e, setfile2Data, setselectedSheet2)
+              }
             ></input>
             <FormControl className={classes.formControl}>
               <InputLabel htmlFor="sheet-from-file2">
@@ -266,7 +256,7 @@ function App() {
               <Select
                 native
                 value={selectedSheet2}
-                onChange={(e) => selectSheetHandler(e, setselectedSheet2)}
+                onChange={(e) => selectOnChangeHandler(e, setselectedSheet2)}
                 inputProps={{
                   id: "sheet-from-file2",
                 }}
@@ -275,56 +265,116 @@ function App() {
                 {sheetFile2Component}
               </Select>
             </FormControl>
-            {headerFile2.length > 0 ? (
-              <List component="nav" aria-label="contacts">
-                <DragSortableList
-                  items={headerFile2}
-                  onSort={onSort}
-                  type="vertical"
-                />
-              </List>
+            {headerFile2Component.length > 0 ? (
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="column-from-file2">
+                  Column as key to compare:
+                </InputLabel>
+                <Select
+                  native
+                  value={selectedColumn2}
+                  onChange={(e) => selectOnChangeHandler(e, setselectedColumn2)}
+                  inputProps={{
+                    id: "column-from-file2",
+                  }}
+                >
+                  <option aria-label="None" value="" />
+                  {headerFile2Component}
+                </Select>
+              </FormControl>
             ) : (
               <></>
             )}
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    {diff2.map((row, idx) => {
-                      if (idx === 0) {
-                        let result = [];
-                        row.forEach((x) => {
-                          result.push(
-                            <TableCell key={x + uuidv4()}>{x}</TableCell>
-                          );
-                        });
-                        return result;
-                      }
-                    })}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {diff2.map((row, idx) => {
-                    let result = [];
-                    if (idx > 0) {
-                      result.push(
-                        <TableRow key={"2" + uuidv4()}>
-                          {row.map((x) => {
-                            result.push(
-                              <TableCell key={x + uuidv4()}>{x}</TableCell>
-                            );
-                          })}
-                        </TableRow>
-                      );
-
-                      return result;
-                    }
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
           </Paper>
         </Grid>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <AppBar position="static" color="secondary">
+                <Tabs value={tabValue} onChange={handleTabChange}>
+                  <Tab label="File 1" />
+                  <Tab label="File 2" />
+                </Tabs>
+              </AppBar>
+              <TabPanel value={tabValue} index={0}>
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        {diff1.map((row, idx) => {
+                          if (idx === 0) {
+                            let result = [];
+                            row.forEach((x) => {
+                              result.push(
+                                <TableCell key={uuidv4()}>{x}</TableCell>
+                              );
+                            });
+                            return result;
+                          }
+                        })}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {diff1.map((row, idx) => {
+                        let result = [];
+                        if (idx > 0) {
+                          result.push(
+                            <TableRow key={"1" + uuidv4()}>
+                              {row.map((x) => (
+                                <TableCell key={x + uuidv4()}>{x}</TableCell>
+                              ))}
+                            </TableRow>
+                          );
+
+                          return result;
+                        }
+                      })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </TabPanel>
+              <TabPanel value={tabValue} index={1}>
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        {diff2.map((row, idx) => {
+                          if (idx === 0) {
+                            let result = [];
+                            row.forEach((x) => {
+                              result.push(
+                                <TableCell key={x + uuidv4()}>{x}</TableCell>
+                              );
+                            });
+                            return result;
+                          }
+                        })}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {diff2.map((row, idx) => {
+                        let result = [];
+                        if (idx > 0) {
+                          result.push(
+                            <TableRow key={"2" + uuidv4()}>
+                              {row.map((x) => (
+                                <TableCell key={x + uuidv4()}>{x}</TableCell>
+                              ))}
+                            </TableRow>
+                          );
+
+                          return result;
+                        }
+                      })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </TabPanel>
+            </Paper>
+          </Grid>
+        )}
         <Grid item xs={12}>
           <Paper className={classes.paper}>
             <Button
